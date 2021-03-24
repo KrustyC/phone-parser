12cbd88919fe91c9dc35d5bb9cf9b6facab92e7b
Davide Crestini <davide.crestini@theculturetrip.com>
2021-03-24 10:12:53 +0000

    fix: added npm script

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const { parseCSV, writeToCSVFile } = require("./csv");
-const { parseNumber } = require("./parse-number");
+const { parseCSV, writeToCSVFile } = require("./src/csv");
+const { parseNumber } = require("./src/parse-number");
 
 const INPUT_FILENAME = "input.csv";
 const OUTPUT_FILENAME = "output.csv";
 
 const CSV_SEPARATOR_INPUT = ";";
 const CSV_SEPARATOR_OUTPUT = ",";
-
-const input = [
-  ["07978150964", "52496493"],
-  ["00163830342", "0426 346588"],
-  ["04043240409", "426,346588"],
-  ["02736140043", "0426-346588"],
-  ["00359020336", "04 26-346588"],
-  ["00721440964", "04/26 34 65 88"],
-  ["01682040983", "052496493"],
-  ["00276050028", "05,2496493"],
-  ["00071150148", "39335250457"],
-  ["06130661009", "335250457"],
-  ["00304350374", "335 250457"],
-];
 
 parseCSV(`${__dirname}/files/${INPUT_FILENAME}`, CSV_SEPARATOR_INPUT)
   .then((rows) => {
